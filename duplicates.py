1da8e1386b7e8615cd985eb7f3758569d63f471d
Serg4356 <asdfg111000@yandex.ru>
2017-12-29 16:49:07 +0500

    Divide find_duplicates() by functionality. Fix bug with file size. Use format() function to handle final message

--- duplicates.py
+++ duplicates.py
@@ -2,18 +2,27 @@ import os
 import sys
 
 
-def find_duplicates(path):
-    file_dict = dict()
+def create_file_location_dictionary(path):
+    file_location_dictionary = {}
     for paths, dirs, files in os.walk(path):
         for file in files:
-            file_dict.setdefault(file + '| size: ' + str(os.path.getsize(path)),[]).append(paths)
-    for key in file_dict.keys():
-        if len(file_dict[key]) > 1:
-            print('File name: ' + key, '| Count of copies: ', len(file_dict[key]), '| Location: ', file_dict[key])
+            file_location_dictionary.setdefault(file + '| size: '
+                                                + str(os.path.getsize(paths + '\\' + file)), []).append(paths)
+    return file_location_dictionary
+
+
+def find_duplicates(file_location_dictionary):
+    duplicates_message = 'Duplicates:\n'
+    for key, value in file_location_dictionary.items():
+        if len(file_location_dictionary[key]) > 1:
+            duplicates_message += ('File name: {} \n Count of copies: {} \n Location: {} \n'.format(key, len(value), value))
+    return duplicates_message
 
 
 if __name__ == '__main__':
+    path_to_folder = input('Enter path to folder:\n')
     try:
-        find_duplicates(sys.argv[1])
+        file_location_dictionary = create_file_location_dictionary(path_to_folder)
+        print(find_duplicates(file_location_dictionary))
     except IndexError:
         print('Please, enter the path to folder')
