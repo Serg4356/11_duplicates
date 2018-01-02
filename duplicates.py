916acdeb9a7a2ecf7fb8ebf5c38ebcb14163178d
Serg4356 <asdfg111000@yandex.ru>
2018-01-02 14:28:20 +0500

    Rename variables, fix PEP8 mistakes, change input() to sys.argv. Delete redundant exception statement

--- duplicates.py
+++ duplicates.py
@@ -4,25 +4,33 @@ import sys
 
 def create_file_location_dictionary(path):
     file_location_dictionary = {}
-    for paths, dirs, files in os.walk(path):
+    for file_paths, dirs, files in os.walk(path):
         for file in files:
-            file_location_dictionary.setdefault(file + '| size: '
-                                                + str(os.path.getsize(paths + '\\' + file)), []).append(paths)
+            file_location_dictionary.setdefault(
+                '{} | size: {}'.format(
+                    file,
+                    str(os.path.getsize('{}\\{}'.format(
+                        file_paths,
+                        file
+                    )))
+                ), []
+            ).append(file_paths)
     return file_location_dictionary
 
 
 def find_duplicates(file_location_dictionary):
     duplicates_message = 'Duplicates:\n'
-    for key, value in file_location_dictionary.items():
-        if len(file_location_dictionary[key]) > 1:
-            duplicates_message += ('File name: {} \n Count of copies: {} \n Location: {} \n'.format(key, len(value), value))
+    for file_name, file_location in file_location_dictionary.items():
+        if len(file_location_dictionary[file_name]) > 1:
+            duplicates_message += (
+                'File name: {} \n Count of copies: {} \n Location: {} \n'.format(
+                    file_name,
+                    len(file_location),
+                    file_location)
+            )
     return duplicates_message
 
 
 if __name__ == '__main__':
-    path_to_folder = input('Enter path to folder:\n')
-    try:
-        file_location_dictionary = create_file_location_dictionary(path_to_folder)
-        print(find_duplicates(file_location_dictionary))
-    except IndexError:
-        print('Please, enter the path to folder')
+    file_location_dictionary = create_file_location_dictionary(sys.argv[1])
+    print(find_duplicates(file_location_dictionary))
