af148fa2f87be893d3399d37bbcfb0f1f596f41e
Serg4356 <asdfg111000@yandex.ru>
2018-01-04 08:34:06 +0500

    Add create_duplicates_message cause Wallie said main() is too complicated

--- duplicates.py
+++ duplicates.py
@@ -13,19 +13,25 @@ def create_file_location_dictionary(path):
     return files_location_dictionary
 
 
+def create_duplicates_message(file_location_dictionary):
+    duplicates_message = ''
+    for file_name, file_locations in file_location_dictionary.items():
+        if len(file_locations) > 1:
+            duplicates_message += 'File name: {}\nCount of copies: {}\nLocation: \n{} \n\n'.format(
+                file_name,
+                len(file_locations),
+                '\n'.join(file_locations)
+            )
+    return duplicates_message
+
+
 if __name__ == '__main__':
     try:
         path_to_dir = sys.argv[1]
-        if not bool(os.path.isdir(path_to_dir)):
+        if not os.path.isdir(path_to_dir):
             sys.exit('Please enter correct path to folder')
         print('Duplicates:\n')
         file_location_dictionary = create_file_location_dictionary(path_to_dir)
-        for file_name, file_locations in file_location_dictionary.items():
-            if len(file_locations) > 1:
-                print('File name: {}\nCount of copies: {}\nLocation: \n{} \n\n'.format(
-                    file_name,
-                    len(file_locations),
-                    '\n'.join(file_locations)
-                ))
+        print(create_duplicates_message(file_location_dictionary))
     except IndexError:
         print('Please, enter path to folder.')
